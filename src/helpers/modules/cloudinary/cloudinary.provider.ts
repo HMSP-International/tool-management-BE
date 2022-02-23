830dd570b46e245f46519179eceb30cf1e56c1cc
pdh <huy@hmsp.com.au>
2022-02-23 11:25:33 +0700

    deploy

--- src/helpers/modules/cloudinary/cloudinary.provider.ts
+++ src/helpers/modules/cloudinary/cloudinary.provider.ts
@@ -1,4 +1,3 @@
-import { ConfigService } from '@nestjs/config';
 import { ConfigOptions, v2 } from 'cloudinary';
 import { CLOUDINARY } from './constants';
 
@@ -6,9 +5,9 @@ export const CloudinaryProvider = {
 	provide: CLOUDINARY,
 	useFactory:
 		(): ConfigOptions => {
-			const cloud_name = 'hmsp-com';
-			const api_key = '882426464554745';
-			const api_secret = 'YlkQfpQ7PmkSE7bvIQW2ROV65H0';
+			const cloud_name = 'hmsp-cloudinary';
+			const api_key = '828185581879961';
+			const api_secret = 'AqglXRwk3NJHljaDGxjTJxJ0oZw';
 			return v2.config({
 				cloud_name,
 				api_key,
